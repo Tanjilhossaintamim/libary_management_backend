1ff1c400ef5e357cec3f3a82f61a146e06e7c1ab
Tanjil Hossain <tanjilhossain845.com>
2023-09-04 18:59:59 +0600

    finshed category api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
-
+from store.views import CategoryViewSet
 
 router = DefaultRouter()
+router.register('categories', CategoryViewSet)
 
 urlpatterns = router.urls
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Category, Customer, Book, Order
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    '''Admin View for Category'''
+
+    list_display = ('id', 'name', 'created_at')
+    list_per_page = 10
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    '''Admin View for Book'''
+
+    list_display = ('id', 'name', 'category')
+    list_per_page = 10
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    '''Admin View for Customer'''
+
+    list_display = ('id', 'user', 'name', 'phone', 'address', 'college')
+    list_per_page = 10
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    '''Admin View for Order'''
+
+    list_display = ('id', 'customer', 'book', 'placed_at')
