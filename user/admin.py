a0839c0889721da3d92664b5d16a6bad0a3aa966
Tanjil Hossain <tanjilhossain845.com>
2023-09-04 15:43:53 +0600

    compleate user model and create first migration

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    '''Admin View for User'''
+
+    list_display = ('id', 'email', 'password')
+    list_per_page = 10
--- user/models.py
+++ user/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('please insert an email !')
+        email = self.normalize_email(email)
+        user = self.model(email=email, password=password)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password):
+        superuser = self.create_user(email=email, password=password)
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.save(using=self.db)
+        return superuser
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
