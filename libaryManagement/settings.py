c042d4e63412600417d1d85dd071718f1760c65a
Tanjil Hossain <tanjilhossain845.com>
2023-09-05 09:06:31 +0600

    add cors header

--- libaryManagement/settings.py
+++ libaryManagement/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'corsheaders',
     'user',
     'store',
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+  
+
+    "http://localhost:5173",
+
 ]
 
 ROOT_URLCONF = 'libaryManagement.urls'
